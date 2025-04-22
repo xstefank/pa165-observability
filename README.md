e3a036e53d8000dc955ef94fcc1ba89609a066b0
xstefank <xstefank122@gmail.com>
2025-04-22 09:40:56 +0200

    Prepare for lecture 2025

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 To correctly run all services you need the following infrastructure:
 
-podman run --rm --name prometheus --network host -v ./prometheus.yml:/etc/prometheus/prometheus.yml:Z prom/prometheus:v2.51.2 --config.file=/etc/prometheus/prometheus.yml
+docker compose up
 
-podman run --rm -p 3000:3000 --network host grafana/grafana:10.4.2
+Or manually:
 
-podman run --rm -p 9411:9411 openzipkin/zipkin:3.3
+podman run --rm --name prometheus --network host -v ./prometheus.yml:/etc/prometheus/prometheus.yml:Z prom/prometheus:v3.3.9 --config.file=/etc/prometheus/prometheus.yml
+
+podman run --rm -p 3000:3000 --network host grafana/grafana:11.6.0
+
+podman run --rm -p 9411:9411 openzipkin/zipkin:3.5.0
 
--- observable-web/src/main/java/com/example/observableweb/observation/ContextHandler.java
+++ observable-web/src/main/java/com/example/observableweb/observation/ContextHandler.java
@@ -2,29 +2,28 @@ package com.example.observableweb.observation;
 
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationHandler;
-import org.springframework.stereotype.Component;
 
-@Component
-public class SimpleHandler implements ObservationHandler<Observation.Context> {
+//@Component
+public class ContextHandler implements ObservationHandler<Observation.Context> {
 
     @Override
     public void onStart(Observation.Context context) {
-        System.out.println("Start " + context.getName());
+        System.out.println("Start " + context.get(String.class));
     }
 
     @Override
     public void onStop(Observation.Context context) {
-        System.out.println("Stop " + context.getName());
+        System.out.println("Stop " + context.get(String.class));
     }
 
     @Override
     public void onError(Observation.Context context) {
-        System.out.println("Error " + context.getName() + ", error: " + context.getError());
+        System.out.println("Error " + context.get(String.class) + ", error: " + context.getError());
     }
 
     @Override
     public void onEvent(Observation.Event event, Observation.Context context) {
-        System.out.println("Event " + context.getName() + ", event " + event);
+        System.out.println("Event " + context.get(String.class) + ", event " + event);
     }
 
     @Override
